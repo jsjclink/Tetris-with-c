6d403faf3f6833a1b26c4a3a02c35997cc0aa1cb
jsjclink <jsjclink@kaist.ac.kr>
2020-02-22 01:10:31 +0900

    printblock(), block created.

---
+++ Tetris/Tetris/tetris.c
@@ -0,0 +1,211 @@
+#include<stdio.h>
+#include<time.h>
+#include<windows.h>
+
+typedef struct _System {
+	int score, level, gameover;
+}System;
+
+void gotoxy(int x, int y);
+void hidecursor();
+void printblock(int(*arr)[4][4][4], int a, int block, int x, int y);
+
+int arr[7][4][4][4] = {
+
+		0,0,0,0,
+		1,1,1,1,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,0,1,0,
+		0,0,1,0,
+		0,0,1,0,
+		0,0,1,0,
+
+		0,0,0,0,
+		0,0,0,0,
+		1,1,1,1,
+		0,0,0,0,
+
+		0,1,0,0,
+		0,1,0,0,
+		0,1,0,0,
+		0,1,0,0,
+
+		1,0,0,0,
+		1,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,1,0,
+		0,1,0,0,
+		0,1,0,0,
+		0,0,0,0,
+
+		0,0,0,0,
+		1,1,1,0,
+		0,0,1,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		0,1,0,0,
+		1,1,0,0,
+		0,0,0,0,
+
+		0,0,1,0,
+		1,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		0,1,0,0,
+		0,1,1,0,
+		0,0,0,0,
+
+		0,0,0,0,
+		1,1,1,0,
+		1,0,0,0,
+		0,0,0,0,
+
+		1,1,0,0,
+		0,1,0,0,
+		0,1,0,0,
+		0,0,0,0,
+
+
+		0,1,1,0,
+		0,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,1,0,
+		0,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,1,0,
+		0,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,1,0,
+		0,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,1,0,
+		1,1,0,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		0,1,1,0,
+		0,0,1,0,
+		0,0,0,0,
+
+		0,0,0,0,
+		0,1,1,0,
+		1,1,0,0,
+		0,0,0,0,
+
+		1,0,0,0,
+		1,1,0,0,
+		0,1,0,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		1,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		0,1,1,0,
+		0,1,0,0,
+		0,0,0,0,
+
+		0,0,0,0,
+		1,1,1,0,
+		0,1,0,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		1,1,0,0,
+		0,1,0,0,
+		0,0,0,0,
+
+		1,1,0,0,
+		0,1,1,0,
+		0,0,0,0,
+		0,0,0,0,
+
+		0,0,1,0,
+		0,1,1,0,
+		0,1,0,0,
+
+		0,0,0,0,
+		1,1,0,0,
+		0,1,1,0,
+		0,0,0,0,
+
+		0,1,0,0,
+		1,1,0,0,
+		1,0,0,0,
+		0,0,0,0
+};
+
+int main(void) {
+	int block = 2;
+	int phase_n = 0; //0,1,2,3
+
+	System g_system;
+	system("chcp 437");
+	clock_t CurTime, OldTime;
+	int x = 10, y = 0;
+	hidecursor();
+
+
+	while (1) {
+		OldTime = clock();
+		system("cls");
+		//Render
+		printblock(arr, phase_n, block, x, y);
+		y++;
+
+		//Frame setting
+		while (1) {
+			CurTime = clock();
+			if (CurTime - OldTime > 330) { break; }
+		}
+
+	}
+}
+
+
+void gotoxy(int x, int y) {
+	COORD CursorPosition = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CursorPosition);
+}
+void hidecursor() {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleHandle, &info);
+}
+void printblock(int(*arr)[4][4][4], int phase, int block, int x, int y) {
+
+	for (int i = 0; i < 4; i++) {
+		gotoxy(x, y);
+		for (int j = 0; j < 4; j++) {
+			if (arr[block][phase][i][j] == 1) {
+				printf("%c", 254);
+			}
+			else {
+				printf("  ");
+			}
+		}
+		y++;
+	}
+}
+
+
