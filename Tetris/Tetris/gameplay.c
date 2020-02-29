b6f1bb697ca4c48d2dc2b7c95f4cc1755afbe984
jsjclink <jsjclink@kaist.ac.kr>
2020-02-29 17:30:58 +0900

    All function has been made!

--- Tetris/Tetris/gameplay.c
+++ Tetris/Tetris/gameplay.c
@@ -7,92 +7,134 @@
 
 //Global Variables Declaration
 int map[20][14];
-int templines[19][12];
+
 int blockset[7][4][4][4];
-int testblock[4][4];
+int block[4][4];
 int block_x[4];
 int block_y[4];
-int tempblock_x[4];
-int tempblock_y[4];
-int turncount = 0;
-int stage = 0;
-int iswaiting = 1;
 int blocktype = 0;
 int blockphase = 0;
+
 int originalpoint_x = 0;
 int originalpoint_y = 0;
+int tempblock_x[4];
+int tempblock_y[4];
+
+int templines[19][12];
 int clearlineposition[5];
 
+int score = 0;
+
 //Functions Declararion
+void GetKeyInput();
+
 void Gotoxy(int x, int y);
 void PrintMap();
-void PrintStatus();
-void PressAnyKey();
-void CreateBlock();
 void PrintBlock();
-void BlockMoveDown();
-int CheckBlockCollisionDown();
-void RemoveBlock();
+void EraseBlock();//name change erase block
+void PrintStatus();
+
+
+
+void CreateBlock();
+void ChooseRandomBlock();
 void AddBlocktoMap();
-void GetKbInput();
+void BlockMoveDown();
 void BlockMoveLeft();
 void BlockMoveRight();
+void RotateBlock();
+
+int CheckBlockCollisionDown();
 int CheckBlockCollisionLeft();
 int CheckBlockCollisionRight();
-void ChooseRandomBlock();
-void RotateBlock();
 int CheckBlockCollisionRotate();
+
 void CheckLineClear();
 void LineClear();
 void RemoveLine(int lineidx);
 void MoveLinesDownward(int lineidx);
 
+/***************************************************************************/
+
 void GamePlay() {
-	//Initialize
+	//Init
+	int framecount = 0;
+
+	clock_t CurTime, OldTime, OldTimeBlockMove;
+	OldTime = clock();
+	OldTimeBlockMove = clock();
+
 	CreateBlock();
-
-	PrintMap();
-	PrintStatus();
-	PrintBlock();
+	
 	while (1) {
-		//init
-		PrintStatus();
+		//Data Update
+		//Keyboard input
+		GetKeyInput();
 
-		//wait
-		_getch();
-		iswaiting = 0;
-		turncount++;
-		PrintStatus();
+		//Implementation before print(BlockMoveDown, LineClear)
+		CurTime = clock();
+		if (CurTime - OldTimeBlockMove > 500) {
+			OldTimeBlockMove = CurTime;
+			//Implementation
+			if (CheckBlockCollisionDown() == 0) {
+				BlockMoveDown();
+			}
+			else {
+				EraseBlock();
+				AddBlocktoMap();
+				CheckLineClear();
+				LineClear();
+				system("cls");
+				CreateBlock();
+				//Add Score
 
-		//keyboardinput
-		GetKbInput();
-		
-		//implement before print
-		if (CheckBlockCollisionDown() == 0) {
-			BlockMoveDown();
+			}
 		}
-		else {
-			RemoveBlock();
-			AddBlocktoMap();
-			CheckLineClear();
-			LineClear();
-			system("cls");
-			stage++;
-			CreateBlock();
-		}
-		
-		//print
+
+		//Print
 		PrintMap();
-		PrintStatus();
 		PrintBlock();
+		PrintStatus();
 
-
-		//terminate
-		Sleep(1000);
-		iswaiting = 1;
-	}//while
+		//Wait
+		while (1) {
+			CurTime = clock();
+			if (CurTime - OldTime > 33) {
+				OldTime = CurTime;
+				break;
+			}
+		}
+		framecount++;
+	}
 }
 
+/***************************************************************************/
+
+void GetKeyInput() {
+	int inp;
+	if (_kbhit()) {
+		inp = _getch();
+		if (inp == 'c') {
+			if (CheckBlockCollisionRotate()==0) {
+				RotateBlock();
+			}
+		}
+		switch (inp) {
+		case 'j': {
+			if (CheckBlockCollisionLeft()) { break; }
+			BlockMoveLeft();
+			break;
+		}
+		case 'l': {
+			if (CheckBlockCollisionRight()) { break; }
+			BlockMoveRight();
+			break;
+		}
+		}
+	}
+}
+
+/***************************************************************************/
 void Gotoxy(int x, int y) {
 	COORD CursorPosition = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CursorPosition);
@@ -109,20 +151,32 @@ void PrintMap() {
 	}
 }
 
+void PrintBlock() {
+	for (int i = 0; i < 4; i++) {
+		Gotoxy(block_x[i], block_y[i]);
+		printf("@");
+	}
+}
+
+void EraseBlock() {
+	for (int i = 0; i < 4; i++) {
+		Gotoxy(block_x[i], block_y[i]);
+		printf("%c", 0);
+	}
+}
+
 void PrintStatus() {
 	Gotoxy(15, 0);
-	printf("stage : %2d", stage);
+	printf("score : %4d", score*100);
 	for (int i = 0; i < 4; i++) {
-		Gotoxy(15, 2*i+1);
+		Gotoxy(15, 2 * i + 1);
 		printf("block_x[%d] : %3d", i, block_x[i]);
 		Gotoxy(15, 2 * i + 2);
 		printf("block_y[%d] : %3d", i, block_y[i]);
 	}
-	Gotoxy(15, 9);
-	printf("turncount : %3d", turncount);
-	Gotoxy(15, 10);
-	printf("iswaiting : %d", iswaiting);
 }
+
+/***************************************************************************/
 
 void CreateBlock() {
 	//initialization
@@ -135,7 +189,7 @@ void CreateBlock() {
 	//find startpoint
 	for (int i = 0; i < 4; i++) { //x
 		for (int j = 0; j < 4; j++) {
-			if (testblock[j][i] == 1) { startpoint_x = i; isfound = 1; break; }
+			if (block[j][i] == 1) { startpoint_x = i; isfound = 1; break; }
 		}
 		if (isfound == 1) { break; }
 	}
@@ -143,7 +197,7 @@ void CreateBlock() {
 
 	for (int i = 0; i < 4; i++) { //y
 		for (int j = 0; j < 4; j++) {
-			if (testblock[i][j] == 1) { startpoint_y = i; isfound = 1; break; }
+			if (block[i][j] == 1) { startpoint_y = i; isfound = 1; break; }
 		}
 		if (isfound == 1) { break; }
 	}
@@ -152,7 +206,7 @@ void CreateBlock() {
 	//find endpoint
 	for (int i = 3; i >= 0; i--) { //x
 		for (int j = 0; j < 4; j++) {
-			if (testblock[j][i] == 1) { endpoint_x = i; isfound = 1; break; }
+			if (block[j][i] == 1) { endpoint_x = i; isfound = 1; break; }
 		}
 		if (isfound == 1) { break; }
 	}
@@ -160,7 +214,7 @@ void CreateBlock() {
 
 	for (int i = 3; i >= 0; i--) { //y
 		for (int j = 0; j < 4; j++) {
-			if (testblock[i][j] == 1) { endpoint_y = i; isfound = 1; break; }
+			if (block[i][j] == 1) { endpoint_y = i; isfound = 1; break; }
 		}
 		if (isfound == 1) { break; }
 	}
@@ -168,23 +222,35 @@ void CreateBlock() {
 	//allocate position to each block
 	for (int i = startpoint_y; i <= endpoint_y; i++) {
 		for (int j = startpoint_x; j <= endpoint_x; j++) {
-			if (testblock[i][j] == 1) {
+			if (block[i][j] == 1) {
 				block_x[count] = j - startpoint_x + startplace_x;
 				block_y[count] = i - startpoint_y + startplace_y;
 				count++;
 			}
 		}
 	}
-	
+
 	//calculate originalpoint
 	originalpoint_x = block_x[0] - startpoint_x;
 	originalpoint_y = block_y[0] - startpoint_y;
 }
 
-void PrintBlock() {
+void ChooseRandomBlock() {
+	srand(time(NULL));
+	blocktype = rand() % 7;
+	blockphase = rand() % 4;
 	for (int i = 0; i < 4; i++) {
-		Gotoxy(block_x[i], block_y[i]);
-		printf("@");
+		for (int j = 0; j < 4; j++) {
+			block[i][j] = blockset[blocktype][blockphase][i][j];
+		}
+	}
+}
+
+void AddBlocktoMap() {
+	for (int i = 0; i < 4; i++) {
+		map[block_y[i]][block_x[i]] = 1;
+		block_x[i] = 0;
+		block_y[i] = 0;
 	}
 }
 
@@ -197,101 +263,27 @@ void BlockMoveDown() {
 	originalpoint_y++;
 }
 
-int CheckBlockCollisionDown() {
-	for (int i = 0; i < 4; i++) {
-		if (map[block_y[i]+1][block_x[i]] == 1) { //한칸 밑에 블럭 있는지 확인
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void RemoveBlock() {
-	for (int i = 0; i < 4; i++) {
-		Gotoxy(block_x[i], block_y[i]);
-		printf("%c", 0);
-	}
-}
-
-void AddBlocktoMap() {
-	for (int i = 0; i < 4; i++) {
-		map[block_y[i]][block_x[i]] = 1;
-		block_x[i] = 0;
-		block_y[i] = 0;
-	}
-}
-
-void GetKbInput() {
-	char inp;
-	inp = _getch();
-	switch (inp) {
-	case 'j':{
-		if (CheckBlockCollisionLeft() == 1) { break; }
-		BlockMoveLeft();
-		break;
-	}
-	case 'l': {
-		if (CheckBlockCollisionRight() == 1) { break; }
-		BlockMoveRight();
-		break;
-	}
-	case 'c': {
-		if (CheckBlockCollisionRotate()) { break; }
-		RemoveBlock();
-		blockphase++;
-		blockphase = blockphase % 4;
-		RotateBlock();
-		break;
-	}
-	}
-}
-
 void BlockMoveLeft() {
+	EraseBlock();
 	for (int i = 0; i < 4; i++) {
-		Gotoxy(block_x[i], block_y[i]);
-		printf("%c", 0);
 		block_x[i]--;
 	}
 	originalpoint_x--;
 }
 
 void BlockMoveRight() {
+	EraseBlock();
 	for (int i = 0; i < 4; i++) {
-		Gotoxy(block_x[i], block_y[i]);
-		printf("%c", 0);
 		block_x[i]++;
 	}
 	originalpoint_x++;
 }
 
-int CheckBlockCollisionLeft() {
-	for (int i = 0; i < 4; i++) {
-		if (map[block_y[i]][block_x[i]-1] == 1) { //한칸 왼쪽에 블럭 있는지 확인
-			return 1;
-		}
-	}
-	return 0;
-}
-int CheckBlockCollisionRight() {
-	for (int i = 0; i < 4; i++) {
-		if (map[block_y[i]][block_x[i]+1] == 1) { //한칸 오른쪽에 블럭 있는지 확인
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void ChooseRandomBlock() {
-	srand(time(NULL));
-	blocktype = rand()%7;
-	blockphase = rand()%4;
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			testblock[i][j] = blockset[blocktype][blockphase][i][j];
-		}
-	}
-}
 void RotateBlock() {
+	EraseBlock();
+	blockphase++;
+	blockphase = blockphase % 4;
+
 	int count = 0;
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
@@ -302,8 +294,36 @@ void RotateBlock() {
 			}
 		}
 	}
+}
 
 
+
+/***************************************************************************/
+int CheckBlockCollisionDown() {
+	for (int i = 0; i < 4; i++) {
+		if (map[block_y[i] + 1][block_x[i]] == 1) { //한칸 밑에 블럭 있는지 확인
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int CheckBlockCollisionLeft() {
+	for (int i = 0; i < 4; i++) {
+		if (map[block_y[i]][block_x[i] - 1] == 1) { //한칸 왼쪽에 블럭 있는지 확인
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int CheckBlockCollisionRight() {
+	for (int i = 0; i < 4; i++) {
+		if (map[block_y[i]][block_x[i] + 1] == 1) { //한칸 오른쪽에 블럭 있는지 확인
+			return 1;
+		}
+	}
+	return 0;
 }
 
 int CheckBlockCollisionRotate() {
@@ -326,10 +346,10 @@ int CheckBlockCollisionRotate() {
 	for (int i = 0; i < 4; i++) { //temp_x, temp_y중에서 map에 충돌하는 것이 있는지 확인
 		if (map[tempblock_y[i]][tempblock_x[i]] == 1) {
 			//blockphase-- 를 하기 위해 +3, %4를 해줌(0에서 --하면 -1이 될 수도 있기 때문에 덧셈만 함)
-			blockphase += 3 ;
+			blockphase += 3;
 			blockphase = blockphase % 4;
 
-			return 1; 
+			return 1;
 		}
 	}
 	//blockphase-- 를 하기 위해 +3, %4를 해줌(0에서 --하면 -1이 될 수도 있기 때문에 덧셈만 함)
@@ -338,6 +358,7 @@ int CheckBlockCollisionRotate() {
 	return 0;
 }
 
+/***************************************************************************/
 void CheckLineClear() {
 	//init
 	int blockcount;
@@ -385,11 +406,10 @@ void MoveLinesDownward(int lineidx) {
 	}
 }
 
-
-
-
+/***************************************************************************/
 
 //Arrays
+
 int map[20][14] = {
 	1,0,0,0,0,0,0,0,0,0,0,0,0,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,1,
